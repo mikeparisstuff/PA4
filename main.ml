9d798af68e69e342d375ceef647e0fdd31088f7d
Michael Paris <mlparis92@gmail.com>
2014-02-26 17:30:53 -0500

    I think builds very simple ast

--- main.ml
+++ main.ml
@@ -1,25 +1,62 @@
 (* type 'a mult_node = T of 'a * 'a mult_node;; *)
 
 (* ANI: identifier = identifier, typ = type *)
-type identifier = I of int * string;;
-type typ = T of int * string;;
-type formals = F of string * typ;;
-type expr = E of int;;
+type identifier = IDENT of int * string;;
+(* type typ = TYPE of int * string;; *)
+type formals = FORMAL of string * identifier;;
+type expr = INT of int
+	| STRING of string;;
 
-type feature = ANI of identifier * typ 
-	| AI of identifier * typ * expr
-	| M of identifier * typ * formals list * expr;;
+type feature = ATTRIBUTE of identifier * identifier * expr option;;
+	(* | METHOD of identifier * identifier * formals list * expr;; *)
 
-type clas = NI of typ * feature list;;
-type program = P of string * clas list;;
-
+type clas = CLASS of identifier * identifier option * feature list;;
+type program = PROGRAM of clas list;;
 
 (* int = line_no, string = identifier name *)
 
 
+(**********  BUILD THE AST FROM FILE  *************)
+let rec feature_list lines num_features = match num_features with
+	0 -> (lines, [])
+|   _ -> match lines with
+			"attribute_no_init" :: line_no :: name :: type_line_no :: t :: rem_lines -> begin
+					let (rem, feature_nodes) = feature_list rem_lines (num_features-1) in
+					let (line_num, type_line_num) = ( (int_of_string line_no), (int_of_string type_line_no)) in
+					(rem, ATTRIBUTE(IDENT(line_num, name), IDENT(type_line_num, t), None) :: feature_nodes );
+				end
+		| 	_ -> ([], [])
+;;
 
+let rec class_list lines num_classes = match num_classes with
+	0 -> []
+|	_ -> match lines with
+			line_no :: name :: "no_inherits" :: num_features :: lines -> begin
+				let (rem_lines, ast_nodes) = feature_list lines (int_of_string num_features) in
+				let line_num = int_of_string line_no in
+				CLASS( IDENT(line_num, name), None, ast_nodes ) :: (class_list rem_lines (num_classes-1));
+			end
+		|   _ -> []
+;;
+
+let ast lst = match lst with
+	num_classes :: lines -> PROGRAM( (class_list lines (int_of_string num_classes) ) )
+|   [] -> PROGRAM( []);;
 
 let print_list lst = List.iter (fun a -> print_string (a ^ "\n")) lst;;
+
+(****************  PRINTING HELPER METHODS *****************)
+(* let rec print_ast ast = match ast with
+| 	PROGRAM(clas_list) -> List.iter print_ast clas_list
+|   CLASS(t, i, feat_list) -> print_ast t
+							  List.iter print_ast feat_list
+|   ATTRIBUTE(ident, typ, exp) -> begin print_ast ident; print_ast typ; end
+|	IDENT(line_no, s) -> print_string s
+|   STRING(s) -> print_string s
+|	INT(i) -> Printf.printf "%d" i
+| 	_ -> print_string "COULD NOT FIND MATCH"
+(* |	T(line_no, t) -> print_string t *)
+;; *)
 
 let filename = match Array.length (Sys.argv) with
 		2 -> Array.get Sys.argv 1
@@ -37,28 +74,15 @@ try
 with
 | _ -> begin
 	let cool_input = (List.rev !lines) in
-	print_list cool_input;
+	(* print_list cool_input; *)
+	let p = ast cool_input in
+	match p with 
+	PROGRAM(class_list) -> match class_list with
+			hd :: rest -> match hd with
+				CLASS(i1, i2, f) -> match i1 with
+					IDENT(line_no, name) -> Printf.printf "%s\n" name
+				|   _ -> print_string "NO MATCH 3"
+			|   _ -> print_string "NO MATCH 2"
+	|   _ -> print_string "NO MATCH"
 end
-
-
-(**********  BUILD THE AST FROM FILE  *************)
-
-let ast lst = match lst with
-	num_classes :: lines -> P("Program", [](* (class_list lines num_classes []) *) )
-|   _ -> P("Program", []);;
-
-(* let rec class_list lst num_classes = match num_classes with
-	0 -> []
-|	_ -> match lst with
-			lin_no :: class_name :: "inherits" :: num_features :: lines -> 
-		|   lin_no :: class_name :: "no_inherits" :: num_feature :: lines ->  *)
-
-
-
-	(* match List.length classes with
-		num_classes -> []
-	|   _ -> match lst with
-			lin_no :: class_name :: "inherits" :: num_features :: lines -> class_list T("inherits", (feature_list lines num_features [])) :: classes )
-		|   lin_no :: class_name :: "no_inherits" :: num_feature :: lines ->  *)
 	
-
--- simplest.cl
+++ simplest.cl
@@ -1,8 +1,6 @@
-class Main inherits IO {
-  my_attribute : Int <- 5 ; 
-  main() : Object { 
-  	let x : Int <- 3 in
-  	let u : Int <- 4 in
-    out_string("Hello, world.\n") 
-  } ;
+class Main {
+  my_attribute : Int ; 
+  -- main() : Object { 
+  --   out_string("Hello, world.\n") 
+  -- } ;
 } ; 
